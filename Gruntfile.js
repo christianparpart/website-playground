0cab69e11d632dbc518af1b3549875ff20127298
Christian Parpart <trapni@gmail.com>
2016-11-29 03:08:53 +0100

    init

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    var serveStatic = require('serve-static');
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        watch: {
+            options: {
+                nospawn: true
+            },
+            less: {
+                files: ['app/styles/*.less'],
+                tasks: ['less:server']
+            }
+        },
+        connect: {
+          options: {
+            protocol: 'http',
+            hostname: 'localhost',
+            keepalive: true,
+            port: 9000,
+            livereload: true,
+            base: 'app'
+          },
+          server: {
+            options: {
+              middleware: function (connect) {
+                return [
+                  require('connect-livereload')(),
+                  connect().use('/bower_components', serveStatic('./bower_components')),
+                  serveStatic('./app')
+                ];
+              }
+            }
+          }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        less: {
+            server: {
+                options: {
+                    paths: ['app/components/bootstrap/less', 'app/styles']
+                },
+                files: {
+                    'app/styles/main.css': 'app/styles/main.less'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('server', function (target) {
+        grunt.task.run([
+            'less:server',
+            'connect:livereload',
+            'open',
+            'watch'
+        ]);
+    });
+};
---
+++ README.md
@@ -0,0 +1,22 @@
+# Website Playground Learning Project
+
+- tools:
+  - bower
+  - grunt
+- frameworks:
+  - bootstrap
+  - jquery
+
+```
+# install required NodeJS packages
+npm install
+
+# install required Bower packageas
+bower install
+
+# start static web server
+grunt connect:server
+
+# auto-recompile assets
+grunt watch
+```
